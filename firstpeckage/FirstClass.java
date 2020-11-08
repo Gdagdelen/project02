c2e4d7505e4e71707f25a6378b7bbfaec3ccf0c6
Gdagdelen <73183975+Gdagdelen@users.noreply.github.com>
2020-11-08 13:59:32 -0300

    Second changes from remote

--- firstpeckage/FirstClass.java
+++ firstpeckage/FirstClass.java
@@ -4,6 +4,8 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		
+		
+		
 		System.out.println("First commit from local master");
 		
 		System.out.println("Second commit from the local master");
@@ -11,6 +13,10 @@ public class FirstClass {
 		System.out.println("Third commit from the local master");
 		
 		System.out.println("First commit from remote master branch");
+		
+		System.out.println("Second commit from the remote master branch");
+		
+		
 		
 	}
 
