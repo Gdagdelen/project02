5a0e52297d49688d7f9a3f2a54618d0c33b60d17
Gdagdelen <73183975+Gdagdelen@users.noreply.github.com>
2020-11-08 13:54:11 -0300

    second commit from the remote master branch

--- firstpeckage/FirstClass.java
+++ firstpeckage/FirstClass.java
@@ -3,13 +3,16 @@ package firstpeckage;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		System.out.println("First commit from local master branch");
+		
+		System.out.println("First commit from local master");
 		
 		System.out.println("Second commit from the local master");
 	
 		System.out.println("Third commit from the local master");
 		
 		System.out.println("First commit from remote master branch");
+		
+		System.out.println("Second commit from the remote master branch");
 		
 	}
 
