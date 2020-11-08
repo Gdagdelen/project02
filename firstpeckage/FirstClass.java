66ceb050149a88f1a4b8eabb2e1b01d17219cecf
Gdagdelen <gokaydagdelen@gmail.com>
2020-11-08 14:35:08 -0300

    fifth commit from local master

--- firstpeckage/FirstClass.java
+++ firstpeckage/FirstClass.java
@@ -2,24 +2,17 @@ package firstpeckage;
 
 public class FirstClass {
 
-	public static void main(String[] args) {	
-		
-		System.out.println("First commit from local master");
-		
-		System.out.println("Second commit from the local master");
-	
-		System.out.println("Third commit from the local master");
-		
-		System.out.println("First commit from remote master branch");
-		
-		System.out.println("Second commit from the remote master branch");
-		
+	public static void main(String[] args) {			
+		System.out.println("First commit from local master");		
+		System.out.println("Second commit from the local master");	
+		System.out.println("Third commit from the local master");		
+		System.out.println("First commit from remote master branch");		
+		System.out.println("Second commit from the remote master branch");		
 		System.out.println("Third commit from the remote master branch");	
 		
 		System.out.println("Here is a merge conflict I created from local master");
 
 		System.out.println("Here is the conflict from the remote master branch");
-
+		System.out.println("Here is another conflict from the local master branch");
 	}
-
 }
